d76207d3f3aa29ba1dc3072677d1cee283539b50
laurent <laurent.blondy1@gmail.com>
2025-02-18 16:46:12 +0100

    release

--- scripts/post-release-all.ts
+++ scripts/post-release-all.ts
@@ -2,8 +2,7 @@
 import { execSync } from "child_process";
 
 try {
-  execSync("git add .");
-  execSync("git commit -m 'release'");
+  execSync("git commit -a -m 'release'");
   execSync("git push");
 } catch (_) {
   console.info("git: nothing to add, nothing to push.");

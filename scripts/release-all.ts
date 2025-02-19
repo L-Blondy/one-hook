e1713813c38ea9bccb5a773e516f7d6e7592af46
laurent <laurent.blondy1@gmail.com>
2025-02-19 17:09:01 +0100

    script

--- scripts/release-all.ts
+++ scripts/release-all.ts
@@ -37,12 +37,3 @@ await Promise.all(
     }
   })
 );
-
-// after all
-try {
-  execSync("git add .");
-  execSync("git commit -m 'release'");
-  execSync("git push");
-} catch (_) {
-  consola.info("git: nothing to add, nothing to push.");
-}

027bafc0a56d4e266f8fddf3b9d3d4cb5eb9e5e2
laurent <laurent.blondy1@gmail.com>
2025-02-20 17:36:29 +0100

    up

--- packages/utils/tsup.config.ts
+++ packages/utils/tsup.config.ts
@@ -4,9 +4,11 @@ import { defineConfig } from "tsup";
 export const buildOptions = tsupBuildOptions({
   emitter: "./src/emitter/index.ts",
   "entries-of": "./src/entries-of/index.ts",
+  invariant: "./src/invariant/index.ts",
   "is-server": "./src/is-server/index.ts",
   "keys-of": "./src/keys-of/index.ts",
   noop: "./src/noop/index.ts",
+  "standard-schema": "./src/standard-schema/index.ts",
   types: "./src/types/index.ts",
   "values-of": "./src/values-of/index.ts",
 });

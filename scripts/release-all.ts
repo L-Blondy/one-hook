de935a9981bd484a1ebade137b120a30aba7e31c
laurent <laurent.blondy1@gmail.com>
2025-02-22 18:08:08 +0100

    release: 0.0.1-beta.5release: 0.0.1-beta.5

--- scripts/release-all.ts
+++ scripts/release-all.ts
@@ -1,39 +1,40 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { execSync } from "child_process";
-import { getAllPackageJsonPaths, readPackageJson } from "./package-json-utils";
-import path from "path";
-import consola from "consola";
+import { execSync } from 'child_process'
+import { getAllPackageJsonPaths, readPackageJson } from './package-json-utils'
+import path from 'path'
+import consola from 'consola'
 
 // before all
-execSync("pnpm run align");
-execSync("git pull");
+execSync('pnpm run align')
+execSync('git pull')
+execSync('pnpm run bumpp')
 
 // release all
 await Promise.all(
-  getAllPackageJsonPaths("./packages").map((packageJsonPath) => {
-    const packagePath = path.join(packageJsonPath, "..");
-    const packageJson = readPackageJson(packageJsonPath);
-    const shortName = packageJson.name.replace("@rebase.io/", "");
-    const tag = packageJson.version.replace(/[.\-0-9]/g, "");
+  getAllPackageJsonPaths('./packages').map((packageJsonPath) => {
+    const packagePath = path.join(packageJsonPath, '..')
+    const packageJson = readPackageJson(packageJsonPath)
+    const shortName = packageJson.name.replace('@rebase.io/', '')
+    const tag = packageJson.version.replace(/[.\-0-9]/g, '')
 
     try {
       execSync(
         [
           `cd ${packagePath}`,
-          "pnpm run build",
-          "pnpm run test",
+          'pnpm run build',
+          'pnpm run test',
           tag
             ? `pnpm publish --quiet --access public --tag ${tag}`
             : `pnpm publish --quiet --access public`,
-        ].join(" && ")
-      );
+        ].join(' && '),
+      )
       consola.box(
-        `✔ Successfully published package:\n  - ${shortName}\n  - ${packageJson.version}`
-      );
+        `✔ Successfully published package:\n  - ${shortName}\n  - ${packageJson.version}`,
+      )
     } catch (_) {
       consola.box(
-        `✖ Nothing published for package:\n  - ${shortName}\n  - ${packageJson.version}`
-      );
+        `✖ Nothing published for package:\n  - ${shortName}\n  - ${packageJson.version}`,
+      )
     }
-  })
-);
+  }),
+)

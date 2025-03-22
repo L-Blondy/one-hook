29a53809659a80d4148081b96598f7acebb5872d
laurent <laurent.blondy1@gmail.com>
2025-03-22 10:42:45 +0100

    styles

--- docs/src/app/page.tsx
+++ docs/src/app/page.tsx
@@ -2,8 +2,12 @@ import Link from 'fumadocs-core/link'
 
 export default function Homepage() {
   return (
-    <div>
-      <Link children="Get Started" href="/docs" />
+    <div className="grid min-h-screen place-content-center">
+      <Link
+        className="bg-fd-primary rounded-full px-4 py-2 text-white"
+        children="Get Started"
+        href="/docs"
+      />
     </div>
   )
 }

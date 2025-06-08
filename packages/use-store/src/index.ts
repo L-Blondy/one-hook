da354a1d6a7ce5c15d1de865f9591662c45b8cd9
laurent <laurent.blondy1@gmail.com>
2025-06-08 23:00:41 +0200

    up

--- packages/use-store/src/index.ts
+++ packages/use-store/src/index.ts
@@ -1,7 +1,9 @@
 export {
   defineStore,
-  type StoreProviderProps,
   type Storage as Store,
+  type DefineStoreOptions,
 } from './define-store'
 export { local, type LocalStorageOptions } from './storage/local'
 export { memory, type MemoryOptions } from './storage/memory'
+export { cookie, type CookieStorageOptions } from './storage/cookie'
+export { session, type SessionStorageOptions } from './storage/session'

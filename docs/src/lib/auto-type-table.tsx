2f1d1d697c79103daca8ba50763918d52c9c5272
laurent <laurent.blondy1@gmail.com>
2025-06-10 13:37:13 +0200

    up

--- docs/src/lib/auto-type-table.tsx
+++ docs/src/lib/auto-type-table.tsx
@@ -7,6 +7,43 @@ import { TableDescription } from './table-description'
 
 const generator = createGenerator()
 
+function logFileStructure() {
+  const fs = require('fs')
+  const path = require('path')
+
+  function walkSync(dir: string, filelist: string[] = []) {
+    const files = fs.readdirSync(dir)
+
+    files.forEach((file) => {
+      const filepath = path.join(dir, file)
+      const stats = fs.statSync(filepath)
+
+      if (stats.isDirectory()) {
+        filelist = walkSync(filepath, filelist)
+      } else {
+        filelist.push(filepath)
+      }
+    })
+
+    return filelist
+  }
+
+  const cwd = process.cwd()
+  console.log('File structure under', cwd)
+  console.log(
+    walkSync(cwd)
+      .map((f) => f)
+      .filter((f) => !f.includes('node_modules'))
+      .filter((f) => !f.includes('vendor-chunks'))
+      .filter((f) => !f.includes('.next'))
+      .join('\n'),
+  )
+
+  console.log({ cwd })
+}
+
+logFileStructure()
+
 type Props = React.ComponentProps<typeof FumaAutoTypeTable> & {
   title?: string
   description?: string

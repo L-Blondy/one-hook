a249d6d51fcb58faab7afccb8b1e360f53a8d88a
laurent <laurent.blondy1@gmail.com>
2025-08-05 12:16:49 +0200

    update docs

--- docs/src/app/page.tsx
+++ docs/src/app/page.tsx
@@ -1,10 +1,7 @@
 'use client'
-import { useDocumentVisibility } from '@1hook/use-document-visibility'
 import Link from 'fumadocs-core/link'
-import { useState } from 'react'
 
 export default function Homepage() {
-  const [tab, setTab] = useState(1)
   return (
     <div className="flex min-h-screen flex-col place-content-center items-center justify-center gap-2">
       <Link
@@ -12,29 +9,6 @@ export default function Homepage() {
         children="Get Started"
         href="/docs"
       />
-      <button
-        className="bg-fd-primary rounded-full px-4 py-2 text-white"
-        onClick={() => setTab((n) => ++n % 3)}
-      >
-        Toggle Tab
-      </button>
-      {tab === 1 ? <Tab1 /> : tab === 2 ? <Tab2 /> : null}
-      {tab ? <TabNull /> : null}
     </div>
   )
-}
-
-function Tab1() {
-  console.log('tab 1', useDocumentVisibility())
-  return <div className="text-2xl">Tab1</div>
-}
-
-function Tab2() {
-  console.log('tab 2', useDocumentVisibility())
-  return <div className="text-2xl">Tab2</div>
-}
-
-function TabNull() {
-  console.log('tab null', useDocumentVisibility())
-  return null
 }

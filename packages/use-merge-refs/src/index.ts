f23aafa39befb9dab9b559677845eab4acb4e13f
laurent <laurent.blondy1@gmail.com>
2025-11-05 17:21:13 +0100

    increase useMergeRefs arity to 4

--- packages/use-merge-refs/src/index.ts
+++ packages/use-merge-refs/src/index.ts
@@ -6,13 +6,14 @@ type AnyRef<T> =
   | undefined
   | null
 
-export function mergeRefs<T, U = T, V = T>(
+export function mergeRefs<T, U = T, V = T, W = T>(
   ref1: AnyRef<T>,
   ref2: AnyRef<U>,
   ref3?: AnyRef<V>,
+  ref4?: AnyRef<W>,
 ) {
   return (val: T | U | V) => {
-    ;[ref1, ref2, ref3].forEach((ref) => {
+    ;[ref1, ref2, ref3, ref4].forEach((ref) => {
       if (!ref) return
       if (typeof ref === 'function') {
         ref(val as any)
@@ -26,13 +27,14 @@ export function mergeRefs<T, U = T, V = T>(
 /**
  * https://one-hook.vercel.app/docs
  */
-export const useMergeRefs = <T, U = T, V = T>(
+export const useMergeRefs = <T, U = T, V = T, W = T>(
   ref1: AnyRef<T>,
   ref2: AnyRef<U>,
   ref3?: AnyRef<V>,
+  ref4?: AnyRef<W>,
 ) => {
   return React.useCallback(
-    (v: T | U | V) => mergeRefs(ref1, ref2, ref3)(v),
-    [ref1, ref2, ref3],
+    (v: T | U | V) => mergeRefs(ref1, ref2, ref3, ref4)(v),
+    [ref1, ref2, ref3, ref4],
   )
 }
